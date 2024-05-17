639ca3d752d26a4a08e3d462f0e2d8064c906446
308866567 <308866567@qq.com>
2024-05-17 22:07:53 +0800

    Update 1084.cpp

--- acwing/DP/数位/1084.cpp
+++ acwing/DP/数位/1084.cpp
@@ -10,18 +10,20 @@ ll a, b, m;
 ll f[111][111]; // 长度i,各位数字和%m等于j的个数
 void init()
 {
-	rep(i, 0, m - 1)
+	rep(i, 0, 102)
 	{
 		f[1][i] = 1;
 	}
 	rep(i, 2, 30)
 	{
-		rep(j, 0, m - 1)
+		rep(j, 0, max((ll)9,m - 1))
 		{
 			rep(k, 0, 9)
 			{
-				f[i][j] += f[i - 1][(j - k + m) % m];
-			}
+				ll t=(j + k + m) % m;
+				if(!t)
+					f[i][j] += f[i - 1][t];
+			}	
 		}
 	}
 }
@@ -35,13 +37,13 @@ ll dfs(ll n)
 		nums.emplace_back(n % 10), n /= 10;
 	ll last = 0; // 数字和
 	// 高位往低位
-	rep(i, nums.size() - 1, 0)
+	rrep(i, nums.size() - 1, 0)
 	{
 		ll x = nums[i];
 		// 首位插入[0,x-1]
 		rep(j, 0, x - 1)
 		{
-			rep(k, 0, m - 1)
+			rep(k, 0, max((ll)9,m - 1))
 			{
 				if ((last + j + k) % m == 0)
 					res += f[i][k];
@@ -51,11 +53,15 @@ ll dfs(ll n)
 		if (!i)
 			res++;
 	}
+
+
 	return res;
 }
 void solve()
 {
 	cin >> a >> b >> m;
+	init();
+		// cout << dfs(b) <<"-----"<< dfs(a - 1) << "\n";
 	cout << dfs(b) - dfs(a - 1) << "\n";
 }
 
