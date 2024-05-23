dee887aa40218744504b1823bc055ab6666ae638
308866567 <308866567@qq.com>
2024-05-23 20:09:09 +0800

    1

--- acwing/DP/数位/1084.cpp
+++ acwing/DP/数位/1084.cpp
@@ -7,62 +7,75 @@ const ll N = 1e6 + 10;
 #define INF 0x3f3f3f3f
 const ll mod = 1e9 + 7;
 ll a, b, m;
-ll f[111][111]; // 长度i,各位数字和%m等于j的个数
+ll f[111][11][111]; // 长度i,首位为j,各位数字和%m等于k的个数
 void init()
 {
-	rep(i, 0, 102)
+	memset(f, 0, sizeof f);
+	rep(i, 0, 9)
 	{
-		f[1][i] = 1;
+		f[1][i][i % m]++;
 	}
-	rep(i, 2, 30)
+	rep(i, 1, 30)
 	{
-		rep(j, 0, max((ll)9,m - 1))
+		rep(j, 0, 9) // 首位
 		{
-			rep(k, 0, 9)
+			rep(k, 0, m - 1)
 			{
-				ll t=(j + k + m) % m;
-				if(!t)
-					f[i][j] += f[i - 1][t];
-			}	
+				// 当前为f[i][j][k]
+				//  第i位填t
+				rep(t, 0, 9)
+				{
+					f[i + 1][t][(t + k + m) % m] += f[i][j][k];
+				}
+			}
 		}
 	}
+	// for (int i = 2; i < 20; i++)
+	// 	for (int j = 0; j <= 9; j++)
+	// 		for (int k = 0; k < m; k++)
+	// 			for (int x = 0; x <= 9; x++)
+	// 				f[i][j][k] += f[i - 1][x][(k - j+m)%m];
 }
 ll dfs(ll n)
 {
 	if (!n)
 		return 1;
-	ll res = 0;
 	vector<ll> nums;
 	while (n)
 		nums.emplace_back(n % 10), n /= 10;
+	ll res = 0;
 	ll last = 0; // 数字和
 	// 高位往低位
 	rrep(i, nums.size() - 1, 0)
 	{
 		ll x = nums[i];
-		// 首位插入[0,x-1]
+		// 当前位插入[0,x-1]
 		rep(j, 0, x - 1)
 		{
-			rep(k, 0, max((ll)9,m - 1))
+			rep(k, 0, m - 1)
 			{
-				if ((last + j + k) % m == 0)
-					res += f[i][k];
+				//不含j是因为，f数组已经算过j的值了
+				if ((last + k + m) % m == 0)
+					res += f[i + 1][j][k];
 			}
 		}
-		last = (last + x) % m;
-		if (!i)
+		// for (int j = 0; j < x; j++)				// 第i位放0~x-1
+		// 	res += f[i + 1][j][(m - last) % m]; // 0~i位，所以一共有i+1位
+
+		last += x;
+		if (!i && last % m == 0)
 			res++;
 	}
-
-
 	return res;
 }
 void solve()
 {
-	cin >> a >> b >> m;
-	init();
-		// cout << dfs(b) <<"-----"<< dfs(a - 1) << "\n";
-	cout << dfs(b) - dfs(a - 1) << "\n";
+	while (cin >> a >> b >> m)
+	{
+		init();
+		// cout << dfs(b) << "-----" << dfs(a - 1) << "\n";
+		cout << dfs(b) - dfs(a - 1) << "\n";
+	}
 }
 
 int main()
--- acwing/DP/数位/1085.cpp
+++ acwing/DP/数位/1085.cpp
@@ -6,12 +6,76 @@ const ll N = 1e6 + 10;
 #define rrep(i, start, end) for (ll i = start; i >= end; i--)
 #define INF 0x3f3f3f3f
 const ll mod = 1e9 + 7;
-ll n;
+ll l, r;
+ll f[111][111];
+void init()
+{
+	rep(i, 0, 9)
+	{
+		if (i == 4)
+			continue;
+		f[1][i] = 1;
+	}
+	rep(i, 1, 20)
+	{
+		rep(j, 0, 9)
+		{
+			if (j == 4)
+				continue;
+			rep(k, 0, 9)
+			{
+				if (k == 4)
+					continue;
+				if (k == 6 && j == 2)
+					continue;
+				f[i + 1][k] += f[i][j];
+			}
+		}
+	}
+}
+ll dfs(ll n)
+{
+	if (!n)
+		return 1;
+	vector<ll> nums;
+	while (n)
+		nums.emplace_back(n % 10), n /= 10;
+
+	ll res = 0, last = 0;
+	rrep(i, nums.size() - 1, 0)
+	{
+		ll x = nums[i];
+		rep(j, 0, x - 1)
+		{
+			if (j == 4)
+				continue;
+			if (last == 6 && j == 2)
+				continue;
+			res += f[i + 1][j];
+		}
+		if (last == 6 && x == 2)
+			break;
+		if (x == 4)
+			break;
+
+		last = x;
+		if (!i)
+			res++;
+	}
+	
+	return res;
+}
 void solve()
 {
-	cin >> n;
+	init();
+	while (cin >> l >> r)
+	{
+		if (l + r == 0)
+			return;
+		// cout << dfs(r) << "-----" << dfs(l - 1) << "\n";
+		cout << dfs(r) - dfs(l - 1) << "\n";
+	}
 }
-
 
 int main()
 {
